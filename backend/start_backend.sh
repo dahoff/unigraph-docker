8bfa8c19070c03fcb53e702f45ae1ac7333a7256
dahoff <760136+dahoff@users.noreply.github.com>
2022-01-15 13:22:19 -0800

    Configure dgraph endpoint

--- backend/start_backend.sh
+++ backend/start_backend.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
+usage()
+{
+    echo "Usage: $0 [-h <dgraph hostname>] [-p <dgraph portNum>]"
+    exit 1
+}
+
+while getopts "h:p:" opt; do
+    case "${opt}" in
+        h) h=$OPTARG ;;
+        p) p=$OPTARG ;;
+        *) usage ;; 
+    esac
+done
+
 cd /opt/unigraph
+
+if [ -n "$h" ]; then
+    sed -i s/localhost/$h/ packages/unigraph-dev-backend/src/index.ts
+fi
+
+if [ -n "$p" ]; then
+    sed -i s/9080/$p/ packages/unigraph-dev-backend/src/index.ts
+fi
+
+
 yarn backend-start
